de4dc2e1ae39aff444ec874a7736ba754f0a9360
Jove723 <jvmarquez723@gmail.com>
2024-09-11 08:35:22 -0500

    yessir

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-# Tu código aquí
+from data import people
+
+def list_user():
+    if not people:
+        print("El directorio está vacío")
+    else:
+        for nombre, numero in people.items():
+            print(f"Nombre: {nombre} Número: {numero} \n +\n")
+
+def add_user():
+    nombre = input("Ingrese el nuevo nombre: ")
+    numero = input("Ingrese el nuevo número de teléfono: ")
+    if nombre and numero in people:
+        print("El cliente ya existe")
+    else:
+        people.append({"Nombre": nombre, "Número": numero})
+    print(f"Usuario {nombre} agregado exitosamente.")
+
+ 
